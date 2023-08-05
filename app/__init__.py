e4234a20f3939c2f844971a18cc8613cf5c6b6d8
Andreyspl <135239288+Andreyspl@users.noreply.github.com>
2023-08-05 14:29:01 -0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)  # Movido para cima para estar disponível para importações
 
-from app import routes
-
 # Após as importações, crie as tabelas no banco de dados
 with app.app_context():
     db.create_all()
+
+from app import routes
